c53d64356ffc7a93bc6017d7dcf9aa0efaf3fe04
hrithik007 <hrithik.jais2013@gmail.com>
2020-06-03 12:53:55 +0530

    updated javvascript!

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ $(document).ready(function(){
 		$(this).addClass("active");
 	});
 });
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+	var input, filter, ul, li, a, i;
+	input = document.getElementById("myInput");
+	filter = input.value.toUpperCase();
+	div = document.getElementById("dropdownItem");
+	li = div.getElementsByTagName("li");
+	for (i = 0; i < li.length; i++) {
+		txtValue = li[i].textContent || li[i].innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
+	}
+}
